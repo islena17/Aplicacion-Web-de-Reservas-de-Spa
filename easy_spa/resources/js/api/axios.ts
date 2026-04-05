a5ec8987ae9cacf643561d1877d0fbc35b28a30d
Islena <islenapolo17@gmail.com>
2026-04-05 17:03:59 +0200

    cambio la configuracion cache y añado axios

---
+++ easy_spa/resources/js/api/axios.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+  withCredentials: true,
+  withXSRFToken: true,
+  headers: {
+    "X-Requested-With": "XMLHttpRequest",
+    "Accept": "application/json",
+  },
+});
+
+export default api;
