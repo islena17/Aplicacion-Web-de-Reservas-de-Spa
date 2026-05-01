a1328e3061905a7f7a469ecffde7a83256dd04f3
Islena <islenapolo17@gmail.com>
2026-05-01 18:18:32 +0200

    le añado el metodo store a usercontroller

--- easy_spa/app/Http/Controllers/Api/WebMaster/UserController.php
+++ easy_spa/app/Http/Controllers/Api/WebMaster/UserController.php
@@ -19,6 +19,24 @@ class UserController extends Controller
         return response()->json($users);
     }
 
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'role_id' => ['required', 'exists:roles,id'],
+            'email' => ['required', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8'],
+        ]);
+
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+
+        return response()->json([
+            'message' => 'Usuario creado correctamente',
+            'data' => $user->load('role'),
+        ], 201);
+    }
+
     public function show(User $user)
     {
         $user->load([
