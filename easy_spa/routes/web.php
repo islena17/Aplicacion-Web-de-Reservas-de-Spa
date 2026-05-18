b2eed9897422a4a345534c8493ec841115837a8a
Islena <78654052+islena17@users.noreply.github.com>
2026-05-18 17:52:42 +0200

    reordenar rutas en web.php y habilitar endpoint de migraciones

--- easy_spa/routes/web.php
+++ easy_spa/routes/web.php
@@ -2,6 +2,19 @@
 
 use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+// Ponemos primero la ruta específica para que Laravel la encuentre de inmediato
+Route::get('/migrar-base-de-datos-easyspa', function () {
+    try {
+        // Forzamos la ejecución de las migraciones
+        Artisan::call('migrate', ['--force' => true]);
+        $output = Artisan::output();
+        return "¡Migraciones ejecutadas con éxito!<br><pre>" . $output . "</pre>";
+    } catch (\Exception $e) {
+        return "Hubo un error al migrar: " . $e->getMessage();
+    }
+});
 
 
 Route::view('/{any}', 'app')->where('any', '.*');
