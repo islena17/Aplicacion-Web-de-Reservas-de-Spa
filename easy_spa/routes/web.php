686a26543993196f035b7bf007e588c8427ad411
Islena <78654052+islena17@users.noreply.github.com>
2026-05-18 20:44:06 +0200

    añadir db:seed forzado en la ruta de migraciones

--- easy_spa/routes/web.php
+++ easy_spa/routes/web.php
@@ -7,12 +7,22 @@ use Illuminate\Support\Facades\Artisan;
 // Ponemos primero la ruta específica para que Laravel la encuentre de inmediato
 Route::get('/migrar-base-de-datos-easyspa', function () {
     try {
-        // Forzamos la ejecución de las migraciones
         Artisan::call('migrate', ['--force' => true]);
-        $output = Artisan::output();
-        return "¡Migraciones ejecutadas con éxito!<br><pre>" . $output . "</pre>";
+        $outputMigrate = Artisan::output();
+        
+        //Ejecutamos los Seeders de la Base de Datos
+        Artisan::call('db:seed', ['--force' => true]);
+        $outputSeed = Artisan::output();
+        
+        return "
+            <h1>¡Proceso completado con éxito!</h1>
+            <h3>Resultado de las Migraciones:</h3>
+            <pre>" . $outputMigrate . "</pre>
+            <h3>Resultado del Seeding:</h3>
+            <pre>" . $outputSeed . "</pre>
+        ";
     } catch (\Exception $e) {
-        return "Hubo un error al migrar: " . $e->getMessage();
+        return "Hubo un error en el proceso: " . $e->getMessage();
     }
 });
 
