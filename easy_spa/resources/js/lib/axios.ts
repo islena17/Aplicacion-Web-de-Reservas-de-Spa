abfb0b9cc2cb2ab24ec53d78c132f6d96756dbae
Islena <islenapolo17@gmail.com>
2026-05-24 20:57:11 +0200

    cambio axios

--- easy_spa/resources/js/lib/axios.ts
+++ easy_spa/resources/js/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000", 
+  baseURL: "https://easyspa.onrender.com", 
   withCredentials: true,
   withXSRFToken: true,
   headers: {
