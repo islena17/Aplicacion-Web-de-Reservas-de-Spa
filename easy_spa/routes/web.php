471fd6fabe88330a431eadc98de87decc4e55926
Islena <islenapolo17@gmail.com>
2026-05-28 13:06:42 +0200

    quitar ruta de forzar la migracion para el despliegue

--- easy_spa/routes/web.php
+++ easy_spa/routes/web.php
@@ -3,36 +3,4 @@ use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 
-
-// Ponemos primero la ruta específica para que Laravel la encuentre de inmediato
-
-Route::get('/migrar-base-de-datos-easyspa', function () {
-
-    try {
-        Artisan::call('migrate', ['--force' => true]);
-
-        $outputMigrate = Artisan::output();
-
-        //Ejecutamos los Seeders de la Base de Datos
-
-        Artisan::call('db:seed', ['--force' => true]);
-
-        $outputSeed = Artisan::output();
-
-        return "
-
-            <h1>¡Proceso completado con éxito!</h1>
-
-            <h3>Resultado de las Migraciones:</h3>
-
-            <pre>" . $outputMigrate . "</pre>
-
-            <h3>Resultado del Seeding:</h3>
-
-            <pre>" . $outputSeed . "</pre>";
-    } catch (\Exception $e) {
-
-        return "Hubo un error en el proceso: " . $e->getMessage();
-    }
-});
 Route::view('/{any}', 'app')->where('any', '.*');
